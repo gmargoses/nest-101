e7176294b7e3f31bc766832c5802739bda67ba02
gmargoses <gabriel.margoses.ext@eulerhermes.com>
2022-03-08 10:53:32 +0100

    chore: changing DB name

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,4 +1,4 @@
 export default {
   mongoUri:
-    'mongodb+srv://gabo:eeWUn7t1v4xlVHek@cluster0.uajre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    'mongodb+srv://gabo:eeWUn7t1v4xlVHek@cluster0.uajre.mongodb.net/itemsDB?retryWrites=true&w=majority',
 };
